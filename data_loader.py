09157c60c66b2a46e3989f68153b063591347725
Eon Kim <kimiron518@gmail.com>
2019-04-15 16:45:36 +0900

    modify data loader and gitignore

--- data_loader.py
+++ data_loader.py
@@ -5,11 +5,10 @@ from sklearn.model_selection import train_test_split
 
 from tensorflow.python.keras.preprocessing.sequence import pad_sequences
 from tensorflow.python.keras.preprocessing.text import Tokenizer
-from tensorflow.python.keras.preprocessing import sequence
 from tensorflow.python.keras.utils import to_categorical
 
 DECODE = {0: 0, 2: 1, 4: 2} # For One Hot Encdoing
-
+MAX_LEN = 1014
 
 def preprocess(contexts):
     tk = Tokenizer(num_words=None, filters='', char_level=True, oov_token='UNK')
@@ -21,9 +20,10 @@ def preprocess(contexts):
             tk.word_index = char_dict.copy()
             tk.word_index[tk.oov_token] = len(char_dict) + 1
     sequence = tk.texts_to_sequences(contexts)
-    sequence_pad = pad_sequences(sequence, maxlen=1014, padding='post')
+    sequence_pad = pad_sequences(sequence, maxlen=MAX_LEN, padding='post')
     data = np.array(sequence_pad, dtype='float32')
-    print(data)
+
+    # print(data)
 
     return data, tk
 
@@ -31,14 +31,14 @@ def preprocess(contexts):
 def decode_label(label):
     return DECODE[int(label)]
 
-def load_dataset(mode='train', VALID_SPLIT=0.1, shuffle=False):
+def load_dataset(mode='train', valid_split=0.1, shuffle=False):
     DATASET_COLUMNS = ["target", "ids", "date", "flag", "user", "text"]
     DATASET_ENCODING = "ISO-8859-1"
     DATA_PATH = './data/'
 
     if mode=='train':
         data_df = pd.read_csv(DATA_PATH + 'training.1600000.processed.noemoticon.csv', encoding=DATASET_ENCODING, names=DATASET_COLUMNS)
-        train_data, tk = preprocess(data_df['text'])
+        train_data, tokenizer = preprocess(data_df['text'])
 
         data_df['target'] = data_df['target'].apply(lambda x: decode_label(x))
 
@@ -47,16 +47,19 @@ def load_dataset(mode='train', VALID_SPLIT=0.1, shuffle=False):
 
         print("Dataset size:", len(data_df))
 
-        train_input, val_input, train_label, val_label = train_test_split(train_data, label_one_hot, test_size=VALID_SPLIT, shuffle=shuffle)
+        train_input, val_input, train_label, val_label = train_test_split(train_data, label_one_hot, test_size=valid_split, shuffle=shuffle)
 
         print(train_label)
 
-        return train_input, val_input, train_label, val_label, tk
+        return train_input, val_input, train_label, val_label, tokenizer
 
     elif mode=='test':
-        data_df = pd.read_csv(DATA_PATH + '', encoding=DATASET_ENCODING, names=DATASET_COLUMNS)
+        data_df = pd.read_csv(DATA_PATH + 'testdata.manual.2009.06.14.csv', encoding=DATASET_ENCODING, names=DATASET_COLUMNS)
+        test_data, tokenizer = preprocess(data_df['text'])
+        
+        print("Dataset size:", len(data_df))
 
-        return
+        return test_data, tokenizer
 
 
 
